24ad3437547205aac1ca9bfbcbf0026f3c0bca38
noxayyyy <aisuru.nox@gmail.com>
2026-05-23 06:23:19 +0500

    forgor

--- include/Constants.h
+++ include/Constants.h
@@ -16,12 +16,14 @@
 #include <sstream>
 #include <string>
 
+namespace std {
 template<typename T>
 inline std::string to_string(T value) {
 	std::ostringstream os;
 	os << value;
 	return os.str();
 }
+} // namespace std
 #endif
 
 #include <SDL2/SDL_rect.h>
